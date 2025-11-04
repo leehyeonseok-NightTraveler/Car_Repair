32389b60f1eb12413106e4e36a10faeda9c0f3aa
leehyeonseok-NightTraveler <fh0306@naver.com>
2025-11-04 15:04:36 +0900

    main test 및 gitignore 예외 처리
    예외 처리 메인

--- src/main/java/com/boot/controller/CustomerHelpController.java
+++ src/main/java/com/boot/controller/CustomerHelpController.java
@@ -3,10 +3,17 @@ package com.boot.controller;
 import com.boot.service.CustomerHelpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomerHelpController {
 
     @Autowired
     private CustomerHelpService service;
+    
+    @GetMapping("/main")
+    public String showMainPage() {
+        // ViewResolver가 /WEB-INF/views/main.jsp로 매핑함
+        return "main";
+    }
 }
