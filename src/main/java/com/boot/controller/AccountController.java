43ad16f1ece6b81f82df8d3a32a67309d2499437
jiho6670 <pmo5065@gmail.com>
2025-11-05 12:43:39 +0900

    registerstore
    registerstore

--- src/main/java/com/boot/controller/AccountController.java
+++ src/main/java/com/boot/controller/AccountController.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @Slf4j
-public class CustomerHelpController {
+public class AccountController {
 
     
     @GetMapping("/main")
--- src/main/java/com/boot/service/AccountServiceImpl.java
+++ src/main/java/com/boot/service/AccountServiceImpl.java
@@ -16,23 +16,9 @@ public class AccountServiceImpl implements AccountService {
     @Autowired
     private AccountDAO accountDAO;
 
-    // 2. PasswordEncoder 주입(@Autowired) 부분 삭제
-    // @Autowired
-    // private PasswordEncoder passwordEncoder; 
-
     @Override
     public void register(AccountDTO accountDTO) {
         log.info("암호화 없이 원본 DTO를 DB로 전달합니다.");
-        
-        // 3. 암호화 로직 전체 삭제
-        /*
-        String rawPassword = accountDTO.getPassword();
-        String encodedPassword = passwordEncoder.encode(rawPassword);
-        accountDTO.setPassword(encodedPassword);
-        */
-        
-        // 4. 폼에서 받은 DTO를 DAO로 그대로 전달하여 DB에 저장
-        // (비밀번호가 "1234"라면 "1234"가 그대로 DB에 저장됨)
         accountDAO.register(accountDTO);
     }
 }
