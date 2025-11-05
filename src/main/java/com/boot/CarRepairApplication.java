aa2e20c1ed763d004da593bd05ee3684f1e3c898
jh0317 <kim031719@naver.com>
2025-11-05 09:15:35 +0900

    수정
    수정

--- src/main/java/com/boot/CarRepairApplication.java
+++ src/main/java/com/boot/CarRepairApplication.java
@@ -1,10 +1,9 @@
 package com.boot;
 
-import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = MybatisAutoConfiguration.class)
+@SpringBootApplication
 public class CarRepairApplication {
 
 	public static void main(String[] args) {
